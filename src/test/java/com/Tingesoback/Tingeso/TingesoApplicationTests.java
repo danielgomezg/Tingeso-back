0f0552b593f05bfe485c84817593a1317e225eb4
danielgomezg <daniel.gomez@usach.cl>
2020-05-13 19:33:31 -0400

    primer commit

---
+++ src/test/java/com/Tingesoback/Tingeso/TingesoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Tingesoback.Tingeso;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TingesoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
